4ca3dc5e73055b311fa2c080ef1bab409f17b332
Matt Lubel <matt_lubel@hotmail.com>
2017-05-20 20:22:23 +0300

    change port number

--- app/src/start_server.js
+++ app/src/start_server.js
@@ -1,6 +1,6 @@
 const app = require('./server.js')
 
-const port = process.env.PORT || 8111
+const port = process.env.PORT || 2000
 
 app.listen(port, function () {
   console.log(`The magic happens on port ${port}!`)
